6b1c8cf4dc3a056ec2e2b50981496ec34d39bab0
Jeremie Aubut <jeremie.aubut@gmail.com>
2016-08-18 19:49:27 -0400

    adding safari notif
    with onesignal

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,4 +5,12 @@
 jQuery(document).ready(function($) {
 			$('.my-slider').unslider();
 		});
+// Do NOT call init twice, use existing init
+OneSignal.push(["init", {
+  /* Your other settings, for example:
+       (e.g.) appId: '...' 
+     Place the following call on the outermost brace
+  */
+  safari_web_id: 'web.onesignal.auto.56640081-e0ac-44c5-9d26-db4b2624f604'
+}]);
 
