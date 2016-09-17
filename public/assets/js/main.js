ec43358a0ea8a5bdda60fa25f612c62cafdbf063
Jeremie Aubut <jeremie.aubut@gmail.com>
2016-09-17 15:41:26 -0400

    snipcart is on

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,23 @@
+Snipcart.api.configure('show_continue_shopping', true);
+
+Snipcart.subscribe('cart.ready', function (cart) {
+  if (cart != null && cart.items && cart.items.length > 0) {
+    $('.snipcart-summary').show();
+  }
+});
+
+Snipcart.subscribe('item.added', function (item) {
+  $('.snipcart-summary').show();
+});
+
+Snipcart.subscribe('item.removed', function (item, items) {
+  cart = Snipcart.api.cart.get();
+  
+  if (cart.items.length <= 0) {
+    $('.snipcart-summary').hide();
+  }
+});
+
 (function(){
   console.log("harp + surge");
 })();
@@ -12,3 +32,4 @@ $('.grid').masonry({
   percentPosition: true
 });
 
+    
